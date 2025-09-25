1b081dbb1ebf0ceedb92a6227af7440b3cfdc317
ahmedayman44 <ahmedayman203011@gmail.com>
2025-09-25 15:41:00 +0300

    create UserRepos

---
+++ lib/features/user/domain/repos/user_repos.dart
@@ -0,0 +1,7 @@
+import 'package:clean_arch/core/errors/failure.dart';
+import 'package:clean_arch/features/user/domain/entites/user_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserRepos {
+  Future<Either<Failure, UserEntity>> getUser();
+}
